1322f9b4fa3b209120dc8fdf1e542abcaa4a7f0b
civilrightscoldcaseactclass <coldcasecrowdsource@gmail.com>
2017-03-17 21:26:22 -0400

    Revert "Update lawmakers.js"
    This reverts commit 80c4d41460737990ffdb4ffbe37728935312e93c.

--- js/lawmakers.js
+++ js/lawmakers.js
@@ -6,26 +6,18 @@ $(function () {
 	$('#lawmaker-list').hide();
 	$('#loadingDiv').hide();
 	
-	var baseUrl = 'https://congress.api.sunlightfoundation.com/committees?apikey=' + SUNLIGHT_API_KEY + '&fields=member_ids&committee_id=';
-	var senateJudiciaryUrl = baseUrl + 'SSJU';
-	var houseJudiciaryUrl = baseUrl + 'HSJU';
+	var judiciaryUrl = 'https://congress.api.sunlightfoundation.com/committees?apikey=' + SUNLIGHT_API_KEY + '&fields=member_ids&committee_id=';
+	var senateJudiciaryUrl = judiciaryUrl + 'SSJU';
+	var houseJudiciaryUrl = judiciaryUrl + 'HSJU';
 
-	var houseOversightUrl = baseUrl + 'HSGO';
-
-	var senateJudiciaryMembers = [];
+	senateJudiciaryMembers = [];
 	var houseJudiciaryMembers = [];
-
-	var houseOversightMembers = [];
 
 	$.get(senateJudiciaryUrl, function (data) {
 		senateJudiciaryMembers = data.results[0].member_ids;
 	});
 	$.get(houseJudiciaryUrl, function (data) {
 		houseJudiciaryMembers = data.results[0].member_ids;
-	});
-
-	$.get(houseOversightUrl, function (data) {
-		houseOversightMembers = data.results[0].member_ids;
 	});
 
 	$.get(baseUrl + 'files/legislative-directors.csv', function (data) {
