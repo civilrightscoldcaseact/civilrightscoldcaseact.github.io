a1490da26360e5313ef7318d4da77c89246becb2
civilrightscoldcaseactclass <coldcasecrowdsource@gmail.com>
2017-03-22 21:30:06 -0400

    Update lawmakers.js

--- js/lawmakers.js
+++ js/lawmakers.js
@@ -31,7 +31,7 @@ $(function () {
 	$.get(baseUrl + 'files/legislative-directors.csv', function (data) {
 		console.log(JSON.stringify(data));
 		var legislativeEmails = {};
-		var mailArray = data.toString()split('\n');
+		var mailArray = data.toString().split('\n');
 		console.log(mailArray);
 		for (var i = 0; i < mailArray.length; i++) {
 			var kv = mailArray[i].split(',');
